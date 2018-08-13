553c2b18f99c97d7271d1768ace9bb80fe841151
wdzhangchukong <weida.zhang@chukong-inc.com>
2018-08-13 10:35:33 +0800

    Restructure code

--- build-runtime.js
+++ build-runtime.js
@@ -1,12 +1,9 @@
-var WEBPACK_DIR_NAME = "config";
-var WEBPACK_NAME = "webpack.config.js";
-var SRC_DIR_NAME = "engine";
-var RES_DIR_NAME = "engine";
-var JSB_ADAPTER_DIR_NAME = "engine";
-var SIGN_DIR_NAME = "sign";
-var GAME_MANIFEST_DIR_NAME = "src";
-var MAIN_JS_NAME = "game.js";
-var GAME_CONFIG_JSONS_NAME = "manifest.json";
+var SRC_DIR_NAME = "";
+var RES_DIR_NAME = "";
+var JSB_ADAPTER_DIR_NAME = "";
+var GAME_MANIFEST_DIR_NAME = "";
+var MAIN_JS_NAME = "main.js";
+var GAME_CONFIG_JSONS_NAME = "game.config.json";
 
 // main.js
 var path = require('path');
@@ -17,7 +14,6 @@ var Hashes = require('./lib/hashes.min.js');
 let RUNTIME_CONFIG;
 var zipRootPath;
 
-function walkDir(dir, fileCb, dirCb, complete) {
 // 获取资源文件
 function getResPath(name) {
     var resPath = path.join(__dirname, "res");
@@ -27,29 +23,6 @@ function getResPath(name) {
 // 遍历 dir
 function walkDir(dir, fileCb, dirCb, nexDir, complete) {
     var dirList = [dir];
-    do {
-        var dirItem = dirList.pop();
-        var list = fs.readdirSync(dirItem);
-        list.forEach(function (file) {
-            var fileFullPath = path.join(dirItem, file);
-            var stat = fs.statSync(fileFullPath);
-            if (stat && stat.isDirectory()) {
-                dirList.push(fileFullPath);
-                dirCb(dirItem, file);
-            } else {
-                fileCb(dirItem, file);
-            }
-        });
-        if (dirList.length <= 0) {
-            complete();
-        }
-    } while (dirList.length > 0);
-}
-
-function zipDir(dir, rootPath, zip, noZipFileList, complete) {
-    var dirList = [dir];
-    var parentPathList = [rootPath];
-    var parentZip = [zip];
     var dirParentList = [path.dirname(dir)];
     do {
         var dirItem = dirList.pop();
@@ -71,6 +44,34 @@ function zipDir(dir, rootPath, zip, noZipFileList, complete) {
             complete();
         }
     } while (dirList.length > 0);
+}
+
+function zipDir(zipObj, dir, destDirPath, noZipFileList, complete) {
+    zipObj = zipObj.folder(destDirPath);
+    var folderParentList = [zipObj];
+    var folderCurrent;
+
+    walkDir(dir, function (parentDir, fileName) {
+        var shouldZip = true;
+        noZipFileList.forEach(function (noZipFile) {
+            if (fileName === noZipFile) {
+                shouldZip = false;
+            }
+        });
+        if (shouldZip) {
+            var fullPath = path.join(parentDir, fileName);
+            addZipFile(folderCurrent, fileName, fullPath);
+        }
+    }, function (parentDir, dirName) {
+        folderParentList.push(folderCurrent);
+    }, function (parentDir, currentDir) {
+        folderCurrent = folderParentList.pop().folder(currentDir.slice(parentDir.length + 1, currentDir.length));
+    }, function () {
+        complete();
+    });
+}
+function addZipFile(zipObj, filePath, fullPath) {
+        zipObj.file(filePath, fs.readFileSync(fullPath));
 }
 
 function mkSubpackageRes(assetsPath, targetPath, complete) {
@@ -140,66 +141,6 @@ function writeConfigFile(subpackageArr, path) {
     var deviceOrientation = RUNTIME_CONFIG.deviceOrientation;
     var runtimeVersion = RUNTIME_CONFIG.runtimeVersion;
     var showStatusBar = RUNTIME_CONFIG.showStatusBar;
-function zipDir(zipObj, dir, destDirPath, noZipFileList, complete) {
-    zipObj = zipObj.folder(destDirPath);
-    var folderParentList = [zipObj];
-    var folderCurrent;
-
-    walkDir(dir, function (parentDir, fileName) {
-        var shouldZip = true;
-        noZipFileList.forEach(function (noZipFile) {
-            if (fileName === noZipFile) {
-                shouldZip = false;
-            }
-        });
-        if (shouldZip) {
-            var fullPath = path.join(parentDir, fileName);
-            addZipFile(folderCurrent, fileName, fullPath);
-        }
-    }, function (parentDir, dirName) {
-        folderParentList.push(folderCurrent);
-    }, function (parentDir, currentDir) {
-        folderCurrent = folderParentList.pop().folder(currentDir.slice(parentDir.length + 1, currentDir.length));
-    }, function () {
-        complete();
-    });
-}
-
-var VIVOExternals = {};
-function addZipFile(zipObj, filePath, fullPath) {
-    var shouldHandleRequire = false;
-    var fileExt = path.extname(fullPath);
-    if (fileExt === ".js") {
-        var relativeToZipPath = fullPath.slice(zipRootPath.length + 1, fullPath.length);
-        // 去除 main.js 以及 jsb-adapter 下除了 index.js 的文件
-        if (relativeToZipPath !== "main.js" &&
-            (relativeToZipPath.indexOf("jsb-adapter") !== 0 || filePath === "index.js")) {
-            VIVOExternals[relativeToZipPath] = "commonjs " + relativeToZipPath;
-            shouldHandleRequire = true;
-        }
-    }
-    if (shouldHandleRequire) {
-        var handleString = fs.readFileSync(fullPath, "utf8");
-        /*
-        若需要处理 require 的问题, 则在这里处理
-        // 这里正则表达式处理 handleString, 处理完成后在添加到压缩列表中*/
-        zipObj.file(filePath, handleString);
-    } else {
-        zipObj.file(filePath, fs.readFileSync(fullPath));
-    }
-}
-
-function zipVIVOExternals(zipObj) {
-    // 生成 webpack.config.js 文件，并添加到 zip 中
-    var webpackName = WEBPACK_NAME;
-    var webpackSource = getResPath(webpackName);
-    var webpackFolder = zipObj.folder(WEBPACK_DIR_NAME);
-    var webpackContent = fs.readFileSync(webpackSource, "utf8");
-    webpackContent = webpackContent.replace("EXTERNALS_PLACEHOLDER", JSON.stringify(VIVOExternals));
-    webpackFolder.file(webpackName, webpackContent);
-}
-
-function writeConfigFile(deviceOrientation, showStatusBar, runtimeVersion, path) {
     var jsonObj = {
         "deviceOrientation": deviceOrientation,
         "showStatusBar": showStatusBar,
@@ -220,22 +161,6 @@ function handleSrc(zipObj) {
     var cfgName = 'game.config.json';
     var projectCgfFile = path.join(Editor.projectPath, cfgName);
     addZipFile(srcFolder, GAME_CONFIG_JSONS_NAME, projectCgfFile);
-}
-
-function handleSign(zipObj) {
-    var folder = zipObj.folder(SIGN_DIR_NAME);
-    // 使用 folder 向 sign 中添加文件
-    // addZipFile(folder...);
-    var folderDebug = folder.folder("debug");
-    var fullPath = getResPath("certificate.pem");
-    addZipFile(folderDebug, "certificate.pem", fullPath);
-    fullPath = getResPath("private.pem");
-    addZipFile(folderDebug, "private.pem", fullPath);
-}
-
-function handlePackage(zipObj) {
-    var fullPath = getResPath("package.json");
-    addZipFile(zipObj, "package.json", fullPath);
 }
 
 function handleDirs(zipObj, dirList, destList, noZipFileList, complete) {
@@ -272,34 +197,16 @@ function onBeforeBuildFinish(event, options) {
     var dirRes = path.join(options.dest, resName);
     var dirSrc = path.join(options.dest, srcName);
     var dirAdapter = path.join(options.dest, jsbAdapterName);
-    var dirSubpackage = path.join(dirSrc, "assets");
-    var dirSign = path.join(options.dest, "sign");
 
-    //判断 res 与 src 是否遍历完成
-    var isResComplete;
-    var isSrcComplete;
-    var isAdapterComplete;
     var jsZip = new JSZip();
 
     // 处理 src 目录
     handleSrc(jsZip);
-    // 处理 sign 目录
-    handleSign(jsZip);
-    // 处理 package.json
-    handlePackage(jsZip);
     // 压缩 res src jsb-adapter 目录
     var dirArray = [dirRes, dirSrc, dirAdapter];
     if (fs.existsSync(dirSign)) {
         dirArray.push(dirSign);
     }
-    handleDirs(jsZip,
-        dirArray,
-        [RES_DIR_NAME, SRC_DIR_NAME, JSB_ADAPTER_DIR_NAME, ""],
-        [[], [], ["jsb-builtin.js"], []],
-        function () {
-            // 生成压缩文件
-            var targetName = options.title + '.cpk';
-            var dirTarget = path.join(options.dest, targetName);
 
     //生成分包
     var dirTargetSubpackage = path.join(options.dest, "subpackages");
@@ -324,54 +231,21 @@ function onBeforeBuildFinish(event, options) {
         });
     }
 
-    //生成压缩文件
-    var jsZip = new JSZip();
-    var zip = function () {
-        var targetName = options.title + '.cpk';
-        var dirTarget = path.join(options.dest, targetName);
-            // 添加 webpack.config.js 文件
-            zipVIVOExternals(jsZip);
+    handleDirs(jsZip,
+        dirArray,
+        [RES_DIR_NAME, SRC_DIR_NAME, JSB_ADAPTER_DIR_NAME, ""],
+        [[], [], ["jsb-builtin.js"], []],
+        function () {
+            // 生成压缩文件
+            var targetName = options.title + '.cpk';
+            var dirTarget = path.join(options.dest, targetName);
 
-        jsZip.generateNodeStream({ type: "nodebuffer", base64: false, compression: 'DEFLATE' })
-            .pipe(fs.createWriteStream(dirTarget))
-            .on('finish', function () {
-                let outTips = Editor.T('EXPORT_ASSET.export_tips', { outPath: dirTarget });
-                Editor.log(outTips);
-                generateSubpackage();
-            });
-    };
-
-    //添加 main.js 文件
-    jsZip.file(mainName, fs.readFileSync(fileMain));
-    //添加 game.config.json 文件
-    jsZip.file(cfgName, fs.readFileSync(projectCgfFile));
-    //添加 res 目录中的文件
-    zipDir(dirRes, options.dest, jsZip, [], function () {
-        isResComplete = true;
-        if (isSrcComplete && isAdapterComplete) {
-            zip();
-        }
-    });
-    //添加 src 目录中的文件
-    zipDir(dirSrc, options.dest, jsZip, ["assets"], function () {
-        isSrcComplete = true;
-        if (isResComplete && isAdapterComplete) {
-            zip();
-        }
-    });
-    //添加 jsb-adapter 目录中的文件
-    zipDir(dirAdapter, options.dest, jsZip, ["jsb-builtin.js"], function () {
-        isAdapterComplete = true;
-        if (isResComplete && isSrcComplete) {
-            zip();
-        }
-    });
             jsZip.generateNodeStream({ type: "nodebuffer", base64: false, compression: 'DEFLATE' })
                 .pipe(fs.createWriteStream(dirTarget))
                 .on('finish', function () {
                     let outTips = Editor.T('EXPORT_ASSET.export_tips', { outPath: dirTarget });
                     Editor.log(outTips);
-                    event.reply()
+                    generateSubpackage();
                 });
         });
 }
@@ -391,7 +265,7 @@ function loadRuntimeSettings(event,options) {
 }
 
 module.exports = {
-    name: 'VIVO 快游戏',
+    name: 'OPPO 快游戏',
     platform: 'runtime',
     extends: Editor.isWin32 ? 'win32' : 'mac',
     buttons: [
@@ -403,7 +277,6 @@ module.exports = {
         'play'(event, options) {
             Editor.Ipc.sendToMain('oppo-runtime-devtools:open', options);
         },
-        'build-finished': onBeforeBuildFinish,
     },
     settings: Editor.url('packages://cpk-publish/build-runtime-ui.js')
 };
